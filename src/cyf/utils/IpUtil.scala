2dfe4bd2bd8441ab2255c509abc1af328281c2c2
cyf123 <516691435@qq.com>
2019-08-21 07:02:36 +0800

    补交

---
+++ src/cyf/utils/IpUtil.scala
@@ -0,0 +1,50 @@
+package cyf.utils
+
+import scala.collection.mutable
+import scala.io.Source
+import IpUtil._
+
+class IpUtil(ip:String)(implicit ipRegex:(String)=>mutable.Map[String,String]) extends MapUtil{
+  private val ipMap=select(ip)
+  def setCountry(newKey:String)=setMap("country",newKey)
+  def setRegion(newKey:String)=setMap("region",newKey)
+  def setCity(newKey:String)=setMap("city",newKey)
+  def setIsp(newKey:String)=setMap("isp",newKey)
+  def setSelf(newKey:String)={outMap+=((newKey,ip));this}
+  def setOthers= {super.setOthers(ipMap);this}
+
+  private def setMap(oldKey:String,newKey:String)={putOneToMap(oldKey,newKey)(ipMap);this}
+  private def select(ip: String) ={
+    implicit val ipArr = toIpArr(ip)
+    ipRegex(
+      if(ipGroup.contains((ipArr(0),ipArr(1)))) find(ipGroup((ipArr(0),ipArr(1))))(1)
+      else find(ipBase)(0)
+    )
+  }
+  private def find(ipMap:Map[Array[Int],String])(index:Int)(implicit ipArr:Array[Int]): String ={
+    val filterMap = ipMap.filter(f=>ipArr(index)==f._1(index))
+    if(filterMap.nonEmpty) find(filterMap)(index+1)
+    else ipMap.find(f=>(f._1(index) to toIpArr(f._2.split("\\s+")(1))(index)).contains(ipArr(index))).get._2
+  }
+}
+object IpUtil{
+  private def toIpArr = (f: String) => for (str <- f.split("\\.")) yield str.toInt
+  private lazy val ipBase = Source.fromURL(getClass.getResource("/resource/ipDataBase.txt")).getLines().map(f=>(toIpArr(f.split("\\s+")(0)),f)).toMap
+  private lazy val ipGroup = ipBase.groupBy(f=>(f._1(0),f._1(1)))
+  private implicit val ipRegex = (strIn:String) => {
+    val in = strIn.split("\\s+");
+    def getMap(str: String*) = mutable.Map("country"->str(0),"region"->str(1),"city"->str(2),"isp"->str(3));
+    val isForeign =".*[^省市].*".r;
+    val isNormal = "(.*省)(.*市)".r;
+    val isSpecial ="(内蒙古)(.*市)".r;
+    in(2) match {
+      case isForeign() =>if(in.length==5) getMap(in(2),in(3),null,in(4)) else getMap(in(2),null,null,in(3))
+      case isNormal(region,city) =>getMap("中国",region,city,in(3))
+      case isSpecial(region,city) =>getMap("中国",region,city,in(3))
+      case _ =>getMap("中国",null,in(2),in(3))}
+  }
+  def apply(ip: String)={
+    if(!ip.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")) throw new RuntimeException("ip格式错误")
+    new IpUtil(ip)
+  }
+}
