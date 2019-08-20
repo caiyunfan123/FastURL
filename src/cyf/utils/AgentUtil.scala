fd54f385a490c273bdfc0193e93211bf5e075131
cyf123 <516691435@qq.com>
2019-08-21 06:57:44 +0800

    更新版本2.0 添加了自定义输出功能

--- src/cyf/utils/AgentUtil.scala
+++ src/cyf/utils/AgentUtil.scala
@@ -1,14 +1,36 @@
 package cyf.utils
 import eu.bitwalker.useragentutils.{Browser, OperatingSystem, UserAgent}
+
+import scala.collection.mutable
 object AgentUtil{
-  private def getAgentMap(v:Any):Map[String,String]= {
+  private def getAgentMap(v:Any)= {
     v match {
-      case a: Browser => Map("bro_name" -> a.getName, "bro_type" -> a.getBrowserType.getName, "bro_group" -> a.getGroup.getName, "bro_manu" -> a.getManufacturer.getName, "bro_eng" -> a.getRenderingEngine.name)
-      case b: OperatingSystem => Map("os_name" -> b.getName, "os_type" -> b.getDeviceType.getName, "os_group" -> b.getGroup.getName, "os_manu" -> b.getManufacturer.getName)
-      case c: UserAgent => getAgentMap(c.getBrowser)++getAgentMap(c.getOperatingSystem) ++ Map("bro_version" -> c.getBrowserVersion.getVersion)
+      case a: Browser => mutable.Map("bro_name" -> a.getName, "bro_type" -> a.getBrowserType.getName, "bro_group" -> a.getGroup.getName, "bro_manu" -> a.getManufacturer.getName, "engine" -> a.getRenderingEngine.name)
+      case b: OperatingSystem => mutable.Map("os_name" -> b.getName, "os_type" -> b.getDeviceType.getName, "os_group" -> b.getGroup.getName, "os_manu" -> b.getManufacturer.getName)
+      case c: UserAgent => mutable.Map("version" -> c.getBrowserVersion.getVersion)
     }
   }
   def getBrowserMap(agent:String)=getAgentMap(UserAgent.parseUserAgentString(agent).getBrowser)
   def getOsMap(agent:String)=getAgentMap(UserAgent.parseUserAgentString(agent).getOperatingSystem)
-  def getAllMap(agent:String)=getAgentMap(UserAgent.parseUserAgentString(agent))
+  def getVersionMap(agent:String)=getAgentMap(UserAgent.parseUserAgentString(agent))
+  def apply(agent: String): AgentUtil = new AgentUtil(agent)
+}
+class AgentUtil(agent: String) extends MapUtil{
+  private lazy val browserMap=AgentUtil.getBrowserMap(agent)++=AgentUtil.getVersionMap(agent)
+  private lazy val OSMap=AgentUtil.getOsMap(agent)
+
+  def setBrowserName(newKey:String)=setMap("bro_name",newKey)(browserMap)
+  def setBrowserType(newKey:String)=setMap("bro_type",newKey)(browserMap)
+  def setBrowserGroup(newKey:String)=setMap("bro_group",newKey)(browserMap)
+  def setBrowserManu(newKey:String)=setMap("bro_manu",newKey)(browserMap)
+  def setOSName(newKey:String)=setMap("os_name",newKey)(OSMap)
+  def setOSType(newKey:String)=setMap("os_type",newKey)(OSMap)
+  def setOSGroup(newKey:String)=setMap("os_name",newKey)(OSMap)
+  def setOSManu(newKey:String)=setMap("os_manu",newKey)(OSMap)
+  def setEngine(newKey:String)=setMap("engine",newKey)(browserMap)
+  def setVersion(newKey:String)=setMap("version",newKey)(browserMap)
+  def setSelf(key:String)={outMap+=((key,agent));this}
+  def setOthers={super.setOthers(browserMap,OSMap);this}
+
+  private def setMap(oldKey:String,newKey:String)(map:mutable.Map[String,String])={putOneToMap(oldKey,newKey)(map);this}
 }
--- src/cyf/utils/FastURL.scala
+++ src/cyf/utils/FastURL.scala
@@ -1,37 +1,29 @@
 package cyf.utils
 
-import com.google.gson.Gson
-
-import scala.collection.JavaConversions.mapAsJavaMap
-import scala.collection.mutable
-
-class FastURL(url:String){
-  private var map=mutable.Map[String,String]()++=FastURL.URLtoMap(url)
-  def toMap=map
-  def toJsonString=FastURL.mapToJson(map)
+class FastURL(url:String)(implicit URLRegex:(String)=>TraversableOnce[(String,String)])extends MapUtil{
+  outMap++=URLRegex(url)
   /**
     * 将Ip解析结果添加到map中
-    * @param keyOrIp    如果ip在map中存在，输入key添加解析结果;否则输入ip值
-    * @param isWithin   如果ip在map中不存在，输入false
-    * @return           map更新之后的FastURL
+    * @param keyOrIp   如果ip在map中存在，输入key添加解析结果;否则输入ip值
+    * @param isWithin  如果ip在map中不存在，输入false
+    * @param f         传入一个AgentUtil对象或IpUtil对象，自由决定输出的内容
+    * @return          ip解析结果添加完成后的FastURL
     */
-  def putIPtoMap(keyOrIp: String, isWithin:Boolean=true)=putAnytoMapByFunction(keyOrIp,isWithin)(FastURL.IPtoMap _)
-  def putAgentToMap(keyOrAgent:String, isWithin:Boolean=true)=putAnytoMapByFunction(keyOrAgent,isWithin)(FastURL.agentToMap _)
-  def putURLtoMap(url:String)=putAnytoMapByFunction(url,false)(FastURL.URLtoMap _)
-  def putAnytoMapByFunction(anyOrKey:String,isWithin:Boolean)(f:String=>Map[String,String])={if(isWithin) map.get(anyOrKey) match {case Some(agent)=>this++=f(agent) case None =>this} else this++=f(anyOrKey)}
-  def empty={map=map.empty;this}
+  def putIPtoMap(keyOrIp: String, isWithin:Boolean=true)(implicit f:String=>MapUtil=FastURL.ipUtil)=putAnytoMapByFunction(keyOrIp,isWithin)(f)
+  def putAgentToMap(keyOrAgent:String, isWithin:Boolean=true)(implicit f:String=>MapUtil=FastURL.agentUtil)=putAnytoMapByFunction(keyOrAgent,isWithin)(f)
+  def putAnytoMapByFunction(anyOrKey:String,isWithin:Boolean)(f:String=>MapUtil)={if(isWithin) outMap.get(anyOrKey) match {case Some(agent)=>this++=f(agent).toMap case None =>this} else this++=f(anyOrKey).toMap}
+
+  def -=(key:String)={outMap-=key;this}
+  def +=(key:String,value:String)={outMap+=((key,value));this}
+  def ++=(traversableOnce: TraversableOnce[(String,String)]) = {outMap++= traversableOnce;this}
   def remove(key:String)=this-=key
-  def -=(key:String)={map-=key;this}
-  def +=(key:String,value:String)={map+=((key,value));this}
-  def ++=(traversableOnce: TraversableOnce[(String,String)]) = {map++= traversableOnce;this}
+  def rename(oldKey:String,newKey:String)={putOneToMap(oldKey,newKey)(outMap);this}
+  def renames(keys: Map[String,String])={keys.foreach(f=>rename(f._1,f._2));this}
 }
 
 object FastURL{
-  private val gson = new Gson()
-  var isActor = false
-  def apply(url: String) =new FastURL(url)
-  def IPtoMap(ip:String)=IpUtil.select(ip)(IpUtil.ipRegex)
-  def agentToMap(agent:String)=AgentUtil.getAllMap(agent)
-  def URLtoMap(url:String)="[?&]([^&]*)=([^&]*)".r.findAllIn(URLDecoder.decodeURIComponent(url)).map(f=>(f.split("=")(0).substring(1),f.split("=")(1))).toMap
-  def mapToJson(map:mutable.Map[String,String])=gson.toJson(mapAsJavaMap(map))
+  implicit def URLRegex(url:String)="[?&]([^&]*)=([^&]*)".r.findAllIn(URLDecoder.decodeURIComponent(url)).map(f=>(f.split("=")(0).substring(1),f.split("=")(1)))
+  implicit val ipUtil=(ip:String)=>IpUtil(ip).setOthers
+  implicit val agentUtil=(agent:String)=>AgentUtil(agent).setOthers
+  def apply(url: String)=new FastURL(url)
 }
